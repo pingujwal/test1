80a8937d892327bf5a94230f78de760f3e318860
Ujwal <60282207+pingujwal@users.noreply.github.com>
2020-04-21 10:07:57 +0545

    virtual environmnet setup and django install

---
+++ virvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\administrator\documents\github\test1\virvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
